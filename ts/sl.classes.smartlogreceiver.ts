d5f4a19cc31e19b1ab7b04bf58b12696bcff72f9
Phil Kunz <github@philkunz.com>
2018-11-11 19:06:53 +0100

    fix(core): update

--- ts/sl.classes.smartlogreceiver.ts
+++ ts/sl.classes.smartlogreceiver.ts
@@ -27,15 +27,17 @@ export class SmartlogReceiver {
   /**
    * handles a authenticated log
    */
-  handleAuthenticatedLog(authenticatedLogPackageArg: ILogPackageAuthenticated) {
+  async handleAuthenticatedLog(authenticatedLogPackageArg: ILogPackageAuthenticated) {
     const authString = authenticatedLogPackageArg.auth;
     const logPackage = authenticatedLogPackageArg.logPackage;
 
     if(authString === plugins.smarthash.sha256FromStringSync(this.passphrase)) {
       // this.smartlogInstance.log('ok', 'Message accepted');
       this.smartlogInstance.handleLogPackage(logPackage);
+      return { status: 'ok' };
     } else {
       this.smartlogInstance.log('error', 'Message rejected because of bad passphrase');
+      return { status: 'error' };
       // console.log(plugins.smarthash.sha256FromStringSync(this.passphrase));
     }
   }
